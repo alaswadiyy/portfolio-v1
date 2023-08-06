27d853e8a1aaae1b90c63c0a59b14ddcd3b33f0e
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-08-06 13:08:01 +0100

    navbar

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { ThemeProvider } from 'next-themes'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+
+
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider enableSystem={true} attribute='class'>
+          <Navbar/>
+          {children}
+        </ThemeProvider>
+        
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h2 className='text-2xl'>Hello world</h2>
+    </main>
+  )
+}
